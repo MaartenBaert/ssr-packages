51a5f0b29131864b7e98b983bda2bcc579ceba48
Marshall Banana <djcj@gmx.de>
2015-02-07 23:41:22 +0100

    bump Standards-Version to 3.9.5 (current)

--- generate-ubuntu.php
+++ generate-ubuntu.php
@@ -36,7 +36,7 @@ function ubuntu_generate_control($packagename, $multiarch, $builddepends, $prede
 	$out .= "Priority: optional\n";
 	$out .= "Maintainer: Maarten Baert (v2) <maarten-baert@hotmail.com>\n";
 	$out .= "Build-Depends: $builddepends\n";
-	$out .= "Standards-Version: 3.9.2\n";
+	$out .= "Standards-Version: 3.9.5\n";
 	$out .= "Homepage: http://www.maartenbaert.be/simplescreenrecorder/\n";
 	$out .= "Vcs-Git: git://github.com/MaartenBaert/ssr.git\n";
 	$out .= "Vcs-Browser: https://github.com/MaartenBaert/ssr\n";
