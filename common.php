fe3ed977ff25357d615e86eacc7d65dd334a2021
Maarten Baert <maarten-baert@hotmail.com>
2015-07-27 13:40:34 +0200

    Regenerate packages.

--- common.php
+++ common.php
@@ -18,7 +18,7 @@ $package_ubuntu_dir = "$packages_dir/package-ubuntu";
 $build_arch_dir = "$packages_dir/private-build-arch";
 $build_ubuntu_dir = "$packages_dir/private-build-ubuntu";
 
-$ubuntuversions = array("precise", "trusty", "utopic", "vivid");
+$ubuntuversions = array("precise", "trusty", "vivid", "wily");
 
 // make all errors fatal
 error_reporting(E_ALL);
